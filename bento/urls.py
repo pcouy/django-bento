e60b93ad270798cfdabb23b938a104d1df78b3e0
Pierre Couy <couy.pierre@gmail.com>
2024-11-07 16:52:52 +0100

    Upgrade to django 5

--- bento/urls.py
+++ bento/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns, url
+from django.urls import url
+from bento import views
 
 
-urlpatterns = patterns('bento.views',
-    url(r'^image-upload/$', 'image_upload', name='bento-image-import'),
-    url(r'^text-upload/$', 'text_upload', name='bento-text-import'),
-    url(r'^text-edit/(?P<name>[\w-]+)/$', 'text_edit', name='bento-text-edit'),
-    url(r'^image-edit/(?P<name>[\w-]+)/$', 'image_edit', name='bento-image-edit'),
-)
+urlpatterns = [
+    re_path(r'^image-upload/$', views.image_upload, name='bento-image-import'),
+    re_path(r'^text-upload/$', views.text_upload, name='bento-text-import'),
+    re_path(r'^text-edit/(?P<name>[\w-]+)/$', views.text_edit, name='bento-text-edit'),
+    urre_path(r'^image-edit/(?P<name>[\w-]+)/$', views.image_edit, name='bento-image-edit'),
+]
