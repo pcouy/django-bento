5e1d987d07142d48d3ca837fe41a15fdaa266ee0
Pierre Couy <couy.pierre@gmail.com>
2024-11-07 16:56:36 +0100

    Typo

--- bento/urls.py
+++ bento/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     re_path(r'^image-upload/$', views.image_upload, name='bento-image-import'),
     re_path(r'^text-upload/$', views.text_upload, name='bento-text-import'),
     re_path(r'^text-edit/(?P<name>[\w-]+)/$', views.text_edit, name='bento-text-edit'),
-    urre_path(r'^image-edit/(?P<name>[\w-]+)/$', views.image_edit, name='bento-image-edit'),
+    re_path(r'^image-edit/(?P<name>[\w-]+)/$', views.image_edit, name='bento-image-edit'),
 ]
